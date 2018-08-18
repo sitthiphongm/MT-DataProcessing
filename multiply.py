2a6d39b0bb6c46a4976202c104cc4643b582315d
Sitthiphong <sitthiphongm@gmail.com>
2018-08-18 23:29:41 +0700

    add lowercase just for the last one

--- multiply.py
+++ multiply.py
@@ -10,13 +10,6 @@ import nltk
 from langdetect import detect
 from src.cleantext import clean_content
 from src.language import language_not_en
-from src.language import language_not_th_en
-from nltk.tokenize import regexp_tokenize
-from nltk.tokenize import sent_tokenize
-from src.sentence import split_sentence_th
-from src.sentence import detok_thai
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import TweetTokenizer
 
 encode_type="UTF-8"
 encoding_t="ISO-8859-1"
@@ -25,6 +18,16 @@ PERCENT_ERROR=10
 input_file=sys.argv[1]
 output_file=sys.argv[2]
 
+# remove for now.
+try:
+    if (sys.argv[3])=='True':
+        print('Convert to Lowercase..')
+        lowercase=True
+    else:
+        lowercase=False
+except:
+    lowercase=False
+
 filelist={}
 with codecs.open(input_file, "r", encoding=encode_type) as sourceFile:
     for line in sourceFile:
@@ -32,16 +35,21 @@ with codecs.open(input_file, "r", encoding=encode_type) as sourceFile:
         # print(tokens[0], tokens[1])
         filelist[tokens[1]]=tokens[0]
 
+print('Clear Output File..')
 open(output_file, 'w').close()
 
 for filename,multiply in filelist.items():
     print(filename, multiply)
+    multiply=int(multiply)
     with codecs.open(filename, "r", encoding=encode_type) as ppfile:
         line_count=0
         content_buff=''
         for line in ppfile:
             text=line.strip()
-            for _ in range(int(multiply)):
+            for i in range(int(multiply)):
+                # lower case for the last one.
+                if i==(multiply-1):
+                    text = text.lower()
                 content_buff = content_buff + text + "\n"
                 line_count += 1
 
@@ -52,6 +60,7 @@ for filename,multiply in filelist.items():
                 file.close()
                 content_buff = ""
                 line_count=0
+
         # Write last chunk.
         file = codecs.open(output_file, "a", encode_type)
         file.write(content_buff)
